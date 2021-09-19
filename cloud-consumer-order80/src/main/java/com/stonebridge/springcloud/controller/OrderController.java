0cad25422a774e0bd50673c11c39ad21c993bf7d
stonebridge <stonebridge@njfu.edu.cn>
2021-09-19 20:04:50 +0800

    first commit

---
+++ cloud-consumer-order80/src/main/java/com/stonebridge/springcloud/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.stonebridge.springcloud.controller;
+
+import com.stonebridge.springcloud.entities.CommonResult;
+import com.stonebridge.springcloud.entities.Payment;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Controller
+public class OrderController {
+
+    private static final String PAYMENT_URL = "http://localhost:8081";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/consumer/payment/create", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult<Payment> creat(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+    }
+
+    @RequestMapping(value = "/consumer/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+}
---
+++ cloud-provider-payment8001/src/main/java/com/stonebridge/springcloud/controller/PaymentController.java
@@ -0,0 +1,42 @@
+package com.stonebridge.springcloud.controller;
+
+import com.stonebridge.springcloud.entities.CommonResult;
+import com.stonebridge.springcloud.entities.Payment;
+import com.stonebridge.springcloud.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@Slf4j
+public class PaymentController {
+
+    @Autowired
+    private PaymentService paymentService;
+
+    @RequestMapping(value = "/payment/create", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        log.info("result:" + result);
+        if (result > 0) {
+            return new CommonResult(200, "插入数据库成功", result);
+        } else {
+            return new CommonResult(500, "插入数据库失败", null);
+        }
+    }
+
+    @RequestMapping(value = "/payment/get/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+        int a=10/4;
+        System.out.println(a);
+        if (payment != null) {
+            return new CommonResult(200, "查询成功", payment);
+        } else {
+            return new CommonResult(200, "查询失败，没有对应记录", null);
+        }
+    }
+}
