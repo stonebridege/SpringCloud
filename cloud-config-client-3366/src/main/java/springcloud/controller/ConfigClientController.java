854df5385720c72c8bdf742e953830f07d497186
stonebridge <stonebridge@njfu.edu.cn>
2021-10-03 12:53:44 +0800

    15.SpringCloud Config分布式配置中心

---
+++ cloud-config-client-3366/src/main/java/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @RequestMapping(value = "/configInfo")
+    @ResponseBody
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
