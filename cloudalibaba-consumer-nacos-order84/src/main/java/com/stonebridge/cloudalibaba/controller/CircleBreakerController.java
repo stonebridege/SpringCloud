27933fef5b34cd1ff2e3f0a704ce5ccfe401af8e
stonebridge <stonebridge@njfu.edu.cn>
2021-10-14 21:34:12 +0800

    21.9.3.4.fallback和blockHandler一起配置

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/stonebridge/cloudalibaba/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/stonebridge/cloudalibaba/controller/CircleBreakerController.java
@@ -1,13 +1,12 @@
 package com.stonebridge.cloudalibaba.controller;
 
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.stonebridge.springcloud.entities.CommonResult;
 import com.stonebridge.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -16,12 +15,13 @@ import javax.annotation.Resource;
 @Slf4j
 public class CircleBreakerController {
     public static final String SERVICE_URL = "http://nacos-payment-provider";
+
     @Resource
     private RestTemplate restTemplate;
 
     @RequestMapping("/consumer/fallback/{id}")
     @ResponseBody
-    @SentinelResource(value = "fallback")
+    @SentinelResource(value = "fallback", fallback = "handlerFallback", blockHandler = "blockHandler")
     public CommonResult<Payment> fallback(@PathVariable Long id) {
         CommonResult<Payment> result = restTemplate.getForObject(SERVICE_URL + "/paymentSQL/" + id, CommonResult.class, id);
         if (id == 4) {
@@ -31,5 +31,18 @@ public class CircleBreakerController {
         }
         return result;
     }
+
+    //本例是fallback
+    public CommonResult handlerFallback(@PathVariable Long id, Throwable e) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult<>(444, "兜底异常handlerFallback,exception内容  " + e.getMessage(), payment);
+    }
+
+    //本例是blockHandler
+    public CommonResult blockHandler(@PathVariable Long id, BlockException blockException) {
+        Payment payment = new Payment(id, "null");
+        return new CommonResult<>(445, "blockHandler-sentinel限流,无此流水: blockException  " + blockException.getMessage(), payment);
+    }
 }
+
 
